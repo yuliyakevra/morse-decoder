496fd9ad0506980da344aa51766b84fc93009093
yuliya_kevra <51132744+yuliyakevra@users.noreply.github.com>
2024-08-13 21:36:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,46 +1,61 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--.."
 };
 
 function decode(expr) {
-    // write your solution here
+  const convertToMorse = (str) => {
+    return str.toUpperCase().split("").map(el => {
+       return MORSE_TABLE[el] ? MORSE_TABLE[el] : el;
+    }).join("");
+ };
+  let morse=convertToMorse(expr);
+    let newString = morse.replaceAll('-', '11').replaceAll('.', '10');
+    let arr=newString.split(' ');
+    function pad(arr) {
+  for (i=0; i<arr.length; i++){
+    if (arr[i].length < 10){
+         while (arr[i].length < 10) arr[i] =arr[i] + "0" ;
+    }
+   if(arr[i].length > 10){
+     arr[i]=arr[i].substring(0,9);
+   }
+       }
+  return arr;
+    
+
 }
 
+let arr1=pad(arr);
+
+
+
+let result=arr1.join('');
+return result;
+}
 module.exports = {
     decode
 }
